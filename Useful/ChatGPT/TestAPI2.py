1ec3d42e02745ada1061fe99a08848024fb6c6e4
Maximilian McClelland <mwmttl@gmail.com>
2023-12-19 18:04:34 -0700

    chatStuff

---
+++ Useful/ChatGPT/TestAPI2.py
@@ -0,0 +1,11 @@
+import openai
+openai.api_key = "key"
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
+  ]
+)
+
+print(completion.choices[0].message.content)
